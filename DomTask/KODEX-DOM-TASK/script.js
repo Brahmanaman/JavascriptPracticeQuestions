574251a644f6275084336bfb3d4db3c6e5f9583b
Aman Sharma <sharma9917375201@gmail.com>
2026-02-25 10:45:29 +0530

    dice game completed

--- DomTask/KODEX-DOM-TASK/script.js
+++ DomTask/KODEX-DOM-TASK/script.js
@@ -1,5 +1,6 @@
 let main = document.querySelector("main")
 let diceContainer = document.querySelector(".diceContainer")
+let h1 = document.createElement("h1")
 
 let leftdiv = document.createElement("div")
 leftdiv.classList.add("leftdiv")
@@ -24,3 +25,26 @@ let btn = document.createElement("button")
 btn.textContent = "Roll the dice";
 
 main.appendChild(btn)
+main.appendChild(h1)
+
+
+btn.addEventListener("click", function () {
+    h1.textContent = "";
+    let rand1 = Math.floor(Math.random() * 6 + 1)
+    leftimg.src = `./assets/dice${rand1}.png`;
+    let rand2 = Math.floor(Math.random() * 6 + 1)
+    rightimg.src = `./assets/dice${rand2}.png`;
+
+
+
+    if (rand1 > rand2) {
+        h1.textContent = "left one is winner"
+    }
+    else if (rand2 > rand1) {
+        h1.textContent = "right one is winner"
+    }
+    else {
+        h1.textContent = "match tied"
+    }
+
+})
